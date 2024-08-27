32d60763779d5d47700f45b48bfde0b14b19906b
Victor Adrian Buica <145504699+victobui@users.noreply.github.com>
2024-08-27 12:30:25 -0400

    Updated with pdf capabilites, changed from tesseract.js to a custom scribe.js with custom vite config ignores

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,6 +10,19 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    target: 'esnext', // Ensure the target is set to a version that supports top-level await
+    minify: false, // Disabling minification might help in avoiding some ESBuild issues
+    esbuild: {
+      target: 'esnext', // Set esbuild target explicitly
+      supported: {
+        'top-level-await': true,  // Ensure top-level await is supported
+      },
+    },
+  },
+  optimizeDeps: {
+    exclude: ['scribe.js-ocr'], // Exclude the problematic dependency
+  },
   test: {
     environment: "jsdom",
     globals: true,
